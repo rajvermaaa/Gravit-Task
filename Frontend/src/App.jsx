1b3f8977aedb5b753828887358c813066b1e6041
rajvermaaa <rajverma0508@outlook.com>
2025-07-25 06:56:14 +0530

    fixing error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,13 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
-
-
-
-
 import Login from "./pages/Login";
-
 import Dashboard from "./pages/Dashboard";
-
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
