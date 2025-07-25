0f057043aaf049e7a1dd2be19f1c80071b7e35bc
rajvermaaa <rajverma0508@outlook.com>
2025-07-25 07:49:28 +0530

    fixing error

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "https://gravitfrontend.vercel.app/",
+    origin: "https://gravitfrontend.vercel.app",
     credentials: true
 }));
 app.use(express.json()); 
