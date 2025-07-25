7fb53019b2c4e53e32cd47d4ad544b0ecf5bdb8f
rajvermaaa <rajverma0508@outlook.com>
2025-07-25 06:01:29 +0530

    deployed

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://gravitfrontend.vercel.app/",
     credentials: true
 }));
 app.use(express.json()); 
--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const authAPI = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://gravit-task.onrender.com",
   withCredentials: true,
 });
 
 const taskAPI = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: "https://gravit-task.onrender.com",
   withCredentials: true,
 });
 
 export { authAPI, taskAPI };
+
+
+// baseURL: "http://localhost:5000/api/auth",
